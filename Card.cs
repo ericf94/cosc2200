82a28e0933cc1f76ac4f85efaea291021f20edd5
ericf94 <eric.fisher@dcmail.ca>
2024-03-14 01:45:17 -0400

    enums

--- Card.cs
+++ Card.cs
@@ -9,6 +9,35 @@ namespace Hearts
 {
     public class Card
     {
+        #region ENUMS
+
+        public enum Suits
+        {
+            Club = 1,
+            Diamond = 2,
+            Heart = 3,
+            Spade = 4,
+        }
+
+        public enum CardNumber
+        {
+            Ace = 1,
+            Two = 2,
+            Three = 3,
+            Four = 4,
+            Five = 5,
+            Six = 6,
+            Seven = 7,
+            Eight = 8,
+            Nine = 9,
+            Ten = 10,
+            Jack = 11,
+            Queen = 12,
+            King = 13,
+        }
+
+        #endregion
+
 
     }
 }
